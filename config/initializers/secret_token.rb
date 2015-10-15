3e81136e3caf74be1d2dc55b4f89dd2ce607a151
Dan_and_Will <Dan_and_Will@appacademy.io>
2015-10-15 11:04:16 -0400

    Init Polls project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Polls::Application.config.secret_key_base = '449ed7815defadc9fbe6215e904f359df435472a6eb251404bff894909784cae7670d998d906bd1d96c443270ce2c9aaf1ca48d7905e6ef40d14a57b99bf30e6'
